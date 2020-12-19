9d5330f03f724566b451cb8460baba2a1088a7a7
Laura Aupert <aupert.laura@gmail.com>
2020-12-19 12:27:31 -0500

    with cocktail ingredients and photo and local storage of cocktails

--- script.js
+++ script.js
@@ -14,13 +14,48 @@ function queryRandomCocktails() {
     method: "GET"
     }).then(function(response) {
       console.log(response)
-      $("#title").text(response.drinks[0].strDrink);
-      var ingredients = $("<p>").text(response.drinks[0].strIngredient1);
-      $("#title").append(ingredients)
+      cocktails.push(response.drinks[0].strDrink)
+      console.log(cocktails)
+    for (i = 0; i < 10; i++) {
+    localStorage.setItem("cocktail" + [i], (JSON.stringify(cocktails[i])))
+    }
+
+    $("#ingredients").empty();
+    var ingredients1 = $("<p>").text(response.drinks[0].strIngredient1 + ": " + response.drinks[0].strMeasure1);
+          $("#ingredients").append(ingredients1)
+          var ingredients2 = $("<p>").text(response.drinks[0].strIngredient2 + ": " + response.drinks[0].strMeasure2);
+          $("#ingredients").append(ingredients2)
+          if (response.drinks[0].strIngredient4 !== null) {
+          var ingredients3 = $("<p>").text(response.drinks[0].strIngredient3 + ": " + response.drinks[0].strMeasure3);
+          $("#ingredients").append(ingredients3)
+          }
+          if (response.drinks[0].strIngredient4 !== null) {
+          var ingredients4 = $("<p>").text(response.drinks[0].strIngredient4 + ": " + response.drinks[0].strMeasure4);
+          $("#ingredients").append(ingredients4)
+          }
+          if (response.drinks[0].strIngredient5 !== null) {
+          var ingredients5 = $("<p>").text(response.drinks[0].strIngredient5 + ": " + response.drinks[0].strMeasure5);
+          $("#ingredients").append(ingredients5)
+          }
+          if (response.drinks[0].strIngredient6 !== null) {
+          var ingredients6 = $("<p>").text(response.drinks[0].strIngredient6 + ": " + response.drinks[0].strMeasure6);
+          $("#ingredients").append(ingredients6)
+          }
+          if (response.drinks[0].strIngredient7 !== null) {
+          var ingredients7 = $("<p>").text(response.drinks[0].strIngredient7 + ": " + response.drinks[0].strMeasure7);
+          $("#ingredients").append(ingredients7)
+          }
+          if (response.drinks[0].strIngredient8 !== null) {
+          var ingredients8 = $("<p>").text(response.drinks[0].strIngredient8 + ": " + response.drinks[0].strMeasure8);
+          $("#ingredients").append(ingredients8)
+          }
+          $(".recipe").text(response.drinks[0].strInstructions)
+
+
       var pic = response.drinks[0].strDrinkThumb
-      var photo = $("#photo").attr("src", pic)
-      $("#title").prepend(photo)
-      console.log(videos)
+      var photo = $(".demo-blog .coffee-pic .mdl-card__media")
+      photo.attr("style", "background-image: url(" + pic + ")")
+
     })
 }
 
