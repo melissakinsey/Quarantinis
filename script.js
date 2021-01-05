822f7e4899bf62323913ad9106a21af525c6dbd4
Laura Aupert <aupert.laura@gmail.com>
2021-01-05 15:48:45 -0500

    JS: cleaned up the code, deleted all comments and console.logs, added a .splice() to the reset function.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-var APIkeyYoutube = configVars.APIkeyYoutube
+var APIkeyYoutube = configVars.APIkeyYoutube;
 
-var queryURLcocktails = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+var queryURLcocktails = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-var searchVideo = []
-var cocktails = []
-var favCocktails = []
+var searchVideo = [];
+var cocktails = [];
+var favCocktails = [];
 
-  renderFavorites();
+renderFavorites();
 
 function queryRandomCocktails() {
     $.ajax({
@@ -16,46 +16,47 @@ function queryRandomCocktails() {
     }).then(function(response) {
       $("#cocktail-name").text(response.drinks[0].strDrink); 
       $("#ingredients").empty();
-      $(".recipe").text(response.drinks[0].strInstructions)
+      $(".recipe").text(response.drinks[0].strInstructions);
       for (i = 0; i < 10; i++) {
         if (Object.values(response.drinks[0])[21+i] !== null && Object.values(response.drinks[0])[21+i] !== "" && Object.values(response.drinks[0])[36+i] !== null &&Object.values(response.drinks[0])[36+i] !== "") {
           var ingredients = $("<p>").text(Object.values(response.drinks[0])[21+i] + ": " + Object.values(response.drinks[0])[36+i])
-          $("#ingredients").append(ingredients)
+          $("#ingredients").append(ingredients);
         } else if (Object.values(response.drinks[0])[21+i] !== null && Object.values(response.drinks[0])[36+i] === null) {
           var ingredients = $("<p>").text(Object.values(response.drinks[0])[21+i]);
-          $("#ingredients").append(ingredients)
+          $("#ingredients").append(ingredients);
         }
       }
-        var pic = response.drinks[0].strDrinkThumb
-        var photo = $(".cocktail-pic")
-        photo.attr("style", "background-image: url(" + pic + ")")
+      var pic = response.drinks[0].strDrinkThumb;
+      var photo = $(".cocktail-pic");
+      photo.attr("style", "background-image: url(" + pic + ")");
     })
 }
 
 function queryCocktailName() {
   var cocktailName =  $("#cocktail-search").val().trim() || cocktails[cocktails.length-1];
-  $("#cocktail-search").val("")
+  $("#cocktail-search").val("");
   if (cocktailName) {
-  var queryURLsearch = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktailName
+  var queryURLsearch = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktailName;
     $.ajax({
     url: queryURLsearch,
     method: "GET"
   }).then(function(response) {
     $("#cocktail-name").text(response.drinks[0].strDrink);        
-    $(".recipe").text(response.drinks[0].strInstructions)
+    $(".recipe").text(response.drinks[0].strInstructions);
     $("#ingredients").empty();
     for (i = 0; i < 10; i++) {
-        if (Object.values(response.drinks[0])[21+i] !== null && Object.values(response.drinks[0])[21+i] !== "" && Object.values(response.drinks[0])[36+i] !== null &&Object.values(response.drinks[0])[36+i] !== "") {
+        if (Object.values(response.drinks[0])[21+i] !== null && Object.values(response.drinks[0])[21+i] !== "" 
+          && Object.values(response.drinks[0])[36+i] !== null &&Object.values(response.drinks[0])[36+i] !== "") {
           var ingredients = $("<p>").text(Object.values(response.drinks[0])[21+i] + ": " + Object.values(response.drinks[0])[36+i])
-          $("#ingredients").append(ingredients)
+          $("#ingredients").append(ingredients);
         } else if (Object.values(response.drinks[0])[21+i] !== null && Object.values(response.drinks[0])[36+i] === null) {
           var ingredients = $("<p>").text(Object.values(response.drinks[0])[21+i]);
-          $("#ingredients").append(ingredients)
+          $("#ingredients").append(ingredients);
       }
     }
-    var pic = response.drinks[0].strDrinkThumb
-    var photo = $(".cocktail-pic")
-    photo.attr("style", "background-image: url(" + pic + ")")
+    var pic = response.drinks[0].strDrinkThumb;
+    var photo = $(".cocktail-pic");
+    photo.attr("style", "background-image: url(" + pic + ")");
   })
 }
 }
@@ -64,39 +65,17 @@ function renderFavorites () {
   for (i = 0; i < localStorage.length; i++) {
     favCocktails.push(JSON.parse(localStorage.getItem(i)))
   }
-  $("#favorites").empty()
+  $("#favorites").empty();
   var newSet = new Set(favCocktails);
   var favCocktailsList = Array.from(newSet);
 
   for (i = localStorage.length; i >= 0; i--) {
     if (favCocktailsList[i] !== null && favCocktailsList[i] !== undefined) {
-    localStorage.setItem(i, JSON.stringify(favCocktailsList[i]));
-     // var favDiv = $("<div>").addClass("fav-div")
-     // var cancel = $("<div>").addClass("cancel-div")
-      
-      var fav = $("<p>").html(favCocktailsList[i]).addClass("favoriteCocktails").attr("data-name", favCocktailsList[i])
-      //var minus = $("<button />").html("cancel").attr("id", i).attr("data-name", favCocktailsList[i]).addClass("minus").addClass("mdl-chip__action").addClass("material-icons")
-      //$("<div>").attr("data-name", favCocktailsList[i]).append(fav).append(minus).appendTo("#favorites")
-      //$("#favorites").append(fav).append(minus)
-      /*
-    var fav = $("<p>").text(favCocktailsList[i]).addClass("favoriteCocktails").attr("data-name", favCocktailsList[i])
-    var minus = $("<button />").html("cancel").attr("id", i).attr("data-name", favCocktailsList[i]).addClass("minus").addClass("mdl-chip__action").addClass("material-icons")*/
-    //var whole = fav.append(minus)
-    //$("#favorites").append(whole)
+      localStorage.setItem(i, JSON.stringify(favCocktailsList[i]));
+      var fav = $("<p>").html(favCocktailsList[i]).addClass("favoriteCocktails").attr("data-name", favCocktailsList[i]);
     }
-    //fav.append(minus)
-    $("#favorites").append(fav)
+    $("#favorites").append(fav);
   }
-  
-
-  /*var favoriteCocktails = JSON.parse(localStorage.getItem("favCocktails"))
-
-    console.log(favoriteCocktails)
-      for (i = 0; i < favoriteCocktails.length; i++) {
-         var fav = $("<p>").text(favoriteCocktails[i]).addClass("favoriteCocktails").attr("data-name", favoriteCocktails[i])
-         console.log(fav)
-         $("#favorites").append(fav)
-      }*/
 }
 
 function queryYoutube() { 
@@ -104,20 +83,17 @@ function queryYoutube() {
   $.ajax({
   url: queryURLyoutube,
   method: "GET"
-}).then(function(response) {
-    var videoSource = "https://www.youtube.com/embed/" + response.items[0].id.videoId + "?autoplay=1"
-    $("#player").attr("src", videoSource)
-})
+  }).then(function(response) {
+    var videoSource = "https://www.youtube.com/embed/" + response.items[0].id.videoId + "?autoplay=1";
+    $("#player").attr("src", videoSource);
+  })
 }
 
 $("#search").on("click", function(event) {
   event.preventDefault();
   queryCocktailName();
-  /*for (i = 0; i < favoriteCocktails.length; i++) {
-    var fav = $("<p>").text(favoriteCocktails[i]).addClass("favoriteCocktails").attr("data-name", favoriteCocktails[i])
-    console.log(fav)
-    $("#favorites").append(fav)}*/
 })
+
 $("#random-cocktails").on("click", function(event) {
   event.preventDefault();
   queryRandomCocktails();
@@ -132,58 +108,25 @@ $(document).on("click", ".music-button", function(event) {
 
 $("#plus-sign").on("click", function (event) {
   event.preventDefault();
-  var newCocktail = $("#cocktail-name").html()
+  var newCocktail = $("#cocktail-name").html();
   if (newCocktail) {
-  favCocktails.push(newCocktail)
-
-  /*newCocktail = $("#cocktail-name").html();
-     if (favCocktails.length >= 5){  
-        favCocktails.shift();
-        favCocktails.push(newCocktail);
-
-    }else{
-    favCocktails.push(newCocktail);*/
-  renderFavorites();
-
+    favCocktails.push(newCocktail);
+    renderFavorites();
   }
-  })
+})
 
-// this function reset the fav list
-  $("#reset").on("click", function(event){
-    event.preventDefault();
-    $("#favorites").empty();
-  localStorage.clear()
-  })
+$("#reset").on("click", function(event){
+  event.preventDefault();
+  $("#favorites").empty();
+  favCocktails.splice(0, favCocktails.length);
+  localStorage.clear();
+})
 
-  /*$(document).on("click", ".minus", function(event) {
-    event.preventDefault();
-    event.stopPropagation()
-   // var element = event.target;
-//    if (element.matches("button") === true) {
-
-    console.log($(this).attr("id"))
-    favCocktails.splice(($(this).attr("data-name")), 1)
-    console.log(favCocktails)
-    localStorage.removeItem($(this).attr("id"))
-    $(this).remove()
-    renderFavorites()
-  //  }
-  })*/
-  
-  //console.log(newCocktail)
-  //localStorage.setItem("favCocktails"+i, JSON.stringify(favCocktails))
-  //console.log(favCocktails)
-
-// this function makes you display the cocktail receipe when you click the favorite cocktail list
 $(document).on("click", ".favoriteCocktails", function(event) {
   event.preventDefault();
-  event.stopPropagation()
- // var element = event.target;
- // if (element.matches("button") !== true) {
-
-  var cocktailName = $(this).attr("data-name")
-  cocktails.push(cocktailName)
-  queryCocktailName()
- // }
+  var cocktailName = $(this).attr("data-name");
+  cocktails.push(cocktailName);
+  queryCocktailName();
 })
+
 })
